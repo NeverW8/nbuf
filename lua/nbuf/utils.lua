daa6deced6efd4ecdc555b4d96eea90877b9d449
NeverW8 <carlerikskantz@gmail.com>
2025-06-09 17:37:24 +0200

    regex bad

--- lua/nbuf/utils.lua
+++ lua/nbuf/utils.lua
@@ -47,33 +47,50 @@ local file_icons = {
   readme = '󰍔',
 }
 
+-- Cache for file icons to avoid repeated calculations
+local icon_cache = {}
+
 function M.get_file_icon(filename)
   if not filename or filename == '' then
     return '󰈙'
   end
   
+  -- Check cache first
+  if icon_cache[filename] then
+    return icon_cache[filename]
+  end
+  
+  local icon = '󰈙' -- default
   local lower_name = filename:lower()
+  
+  -- Check special files first (most common cases)
   if lower_name == 'dockerfile' then
-    return file_icons.dockerfile
+    icon = file_icons.dockerfile
   elseif lower_name == 'makefile' then
-    return file_icons.makefile
+    icon = file_icons.makefile
   elseif lower_name == 'cmakelists.txt' then
-    return file_icons.cmake
-  elseif lower_name:match('%.gitignore') then
-    return file_icons.gitignore
-  elseif lower_name:match('license') then
-    return file_icons.license
-  elseif lower_name:match('readme') then
-    return file_icons.readme
+    icon = file_icons.cmake
+  else
+    -- Check for extensions (faster than regex)
+    local dot_pos = filename:find('%.[^%.]*$')
+    if dot_pos then
+      local ext = filename:sub(dot_pos + 1):lower()
+      icon = file_icons[ext] or '󰈙'
+    else
+      -- Only use regex for special cases without extensions
+      if lower_name:find('gitignore') then
+        icon = file_icons.gitignore
+      elseif lower_name:find('license') then
+        icon = file_icons.license
+      elseif lower_name:find('readme') then
+        icon = file_icons.readme
+      end
+    end
   end
   
-  local ext = filename:match('%.([^%.]+)$')
-  if ext then
-    ext = ext:lower()
-    return file_icons[ext] or '󰈙'
-  end
-  
-  return '󰈙'
+  -- Cache the result
+  icon_cache[filename] = icon
+  return icon
 end
 
 return M
